a790aa84bcc40a28e7449f46c11d465b17422ad8
mete <metekaba96@gmail.com>
2021-02-22 18:09:20 +0300

    Post showing bug has been fixed.

--- CorkBoardUI/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ CorkBoardUI/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -83,7 +83,7 @@ namespace CorkBoardUI.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email };
+                var user = new ApplicationUser { FirstName = Input.FirstName, LastName = Input.LastName, UserName = Input.Email, Email = Input.Email };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
